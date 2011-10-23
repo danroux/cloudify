990139e418666fee325dbf318be18c2f4d569e59
Amed Rodriguez <amed@tractical.com>
2011-10-23 12:08:59 -0500

    generators

---
+++ test/install_generator_test.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class InstallGeneratorTest < Rails::Generators::TestCase
+  tests CloudStorageSync::InstallGenerator
+  destination File.expand_path("../tmp", File.dirname(__FILE__))
+  setup :prepare_destination
+  setup :run_generator
+
+  test "Assert all files are properly created" do
+    assert_file "config/cloud_storage_sync.yml"
+    assert_file "lib/tasks/cloud_storage_sync.rake"
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+require "test/unit"
+require "rubygems"
+require "rails"
+require File.join(File.dirname(__FILE__), '../', 'lib', 'generators', 
+  'cloud-storage-sync', 'install_generator')
