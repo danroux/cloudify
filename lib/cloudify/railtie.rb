6ecd9c782dfb354a63f715c4cbfb3854d6340c44
Daniel Roux <daniel.roux@gmail.com>
2011-11-23 00:11:07 -0600

    * VERSION = "0.0.7" 	- Removed a conditional that made the generator only to work on Rails >= "3.1" 	- Added templates/README & now runs when you run the install the generator. 	- Removed railtie_name from railtie.rb because it's depracated.

--- lib/cloudify/railtie.rb
+++ lib/cloudify/railtie.rb
@@ -1,7 +1,5 @@
 module Cloudify
   class Railtie < Rails::Railtie
-    railtie_name :cloud_storage_sync
-
     rake_tasks do
       load "tasks/deploy.rake"
     end
--- lib/generators/cloudify/install/install_generator.rb
+++ lib/generators/cloudify/install/install_generator.rb
@@ -1,18 +1,19 @@
-require 'rails'
-if ::Rails.version >= "3.1"
-  module Cloudify
-    class InstallGenerator < Rails::Generators::Base
-      desc "This generator installs the files needed for Cloudify configuration"
+module Cloudify
+  class InstallGenerator < Rails::Generators::Base
+    desc "This generator installs the files needed for Cloudify configuration"
 
-      source_root File.expand_path(File.join(File.dirname(__FILE__), '../templates'))
+    source_root File.expand_path(File.join(File.dirname(__FILE__), '../templates'))
 
-      def app_name
-        @app_name ||= Rails.application.is_a?(Rails::Application) && Rails.application.class.name.sub(/::Application$/, "").downcase
-      end
-
-      def generate_initializer
-        template "cloudify.rb", "config/initializers/cloudify.rb"
-      end
+    def app_name
+      @app_name ||= Rails.application.is_a?(Rails::Application) && Rails.application.class.name.sub(/::Application$/, "").downcase
     end
+
+    def generate_initializer
+      template "cloudify.rb", "config/initializers/cloudify.rb"
+    end
+
+    def show_readme
+      readme 'README'
+    end 
   end
 end
